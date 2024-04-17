553a7e3a0102b2327aa34b6286992050bcdf5aeb
rashad <rashadchowki42@gmail.com>
2024-04-18 00:12:36 +0530

    first commit

---
+++ src/test/java/com/thbs/serviceregistry/ServiceregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thbs.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
